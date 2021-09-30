62a844ebcb1171c6085828b19d950f017692095a
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-09-30 10:56:38 +0200

    f

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - return 0
+ *
+ * Description - main return 0
+ * puts - print
+ * Return: 0
+*/
+
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return(0);
+}
