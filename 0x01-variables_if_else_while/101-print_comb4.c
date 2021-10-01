cb280640c6a6043597afb11c647294440be2f526
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-01 14:05:34 +0200

    essai 2 task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,28 +10,27 @@
 
 int main(void)
 {
-	int i;
-	int j;
-	int k;
+	int i, j, k, l;
 
-for (i = 0; i < 10; i++)
-{
-	for (j = i + 1 ; j < 10; j++)
+	for (i = 0; i < 1000; i++)
 	{
-		for (k = j + 1 ; k < 10; k++)
-		{
-		putchar (i);
-		putchar (j);
-		putchar (k);
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 10; /* singles */
 
-		if (i != 7 || j != 8 || k != 9)
+		if (j < k && k < l)
 		{
-			putchar (',');
-			putchar (' ');
-		}
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-}
 putchar('\n');
 return (0);
 }
