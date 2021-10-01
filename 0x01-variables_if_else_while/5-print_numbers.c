b976eb7dcf26919364d72c6772c927ec1e6919b8
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-01 12:07:56 +0200

    all mandatory task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - return 0
+ *
+ * Description - main return 0
+ * printf - Print all single digit numbers of base 10
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
