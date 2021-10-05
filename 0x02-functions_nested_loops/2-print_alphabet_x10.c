1ab75d4af52ec40ce5cded1de719b89e8c8ee634
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-05 12:53:54 +0200

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write a function that prints the alphabet x10, in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 0;
+	char c;
+
+	while (n <= 9)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
