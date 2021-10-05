0cc97f93040da69b140406e42e5f97c302e9049c
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-05 13:03:19 +0200

    corection betty et task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - write a function that prints the alphabet x10, in lowercase
+ * print_alphabet_x10 - function that prints the alphabet x10, in lowercase
  */
 
 void print_alphabet_x10(void)
