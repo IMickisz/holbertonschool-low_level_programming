03140cafac9a00a1ba4a0ee9ee936a7a48670f99
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-05 12:16:38 +0200

    test 1 task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * main - return 0
+ *
+ * Description - main return 0
+ * putchar - Print "_putchar"
+ * Return: 0
+*/
+
+int main(void)
+{
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+        putchar ('\n');
+        return (0);
+}
