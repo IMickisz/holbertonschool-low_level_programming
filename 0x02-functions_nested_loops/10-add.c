036cc382f950d0cfb64a3c8192d5aa1be7dbe2c4
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-05 13:48:42 +0200

    first commit in a while

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - writes a function that adds two integers
+ * @f: first addend
+ * @s: second addend
+ *
+ * Return: the sum
+ */
+
+int add(int f, int s)
+{
+	return (f + s);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int row, n;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			if (n == 0)
+			{
+				_putchar(n + '0');
+			}
+			else if (n * row <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(n * row + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((n * row) / 10 + '0');
+				_putchar((n * row) % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
