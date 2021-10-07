f2987748d13c79b24c785e22bd9f178a77ce6a6d
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-07 15:54:39 +0200

    task 11

---
+++ 0x03-debugging/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - find and print the largest prime factor
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	long int div=2, ans = 0, maxFact;
+
+	while(n!=0)
+	{
+		if(n % div !=0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+		}
+		if(n == 1)
+		{
+			printf("%d is the largest prime factor !",maxFact);
+			ans = 1;
+			break;
+		}
+	}
+	return (0);
+}
