a3ad967b16522991204b7017b48aa111ce399d6c
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-07 12:38:46 +0200

    betty corrections

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@
 
 void print_most_numbers(void)
 {
-	char c = 0;
+	int n;
 
-	while (c <= '9')
+	for (n = '0' ; n <= '9' ; n++)
 	{
-		if (i != '2' || j != '4')
-			_putchar(c);
-		c++;
+		if (n != '2' && n != '4')
+			_putchar (n);
 	}
-	_putchar('\n');
-	return (0);
+	_putchar ('\n');
 }
