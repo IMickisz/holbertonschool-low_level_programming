ef26880b10f32d98d531c9c1aa9726bd74d73daa
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-07 12:25:25 +0200

    correction betty

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,12 +8,12 @@
 
 void print_numbers(void)
 {
-        int i;
+	int i;
 
-        for (i = 0; i < 10; i++)
-        {
-                _putchar(i);
-        }
-        _putchar('\n');
-        return (0);
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
 }
