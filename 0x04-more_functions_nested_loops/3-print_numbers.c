ba95155d24b204eb5e408b79b8b0f22c492bedec
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-07 12:29:42 +0200

    betty corrections

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - function that print numbers from 0 to 9.
  *
- * Return: 0
+ * Return : void
  */
 
 void print_numbers(void)
@@ -11,9 +11,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-	{
 		_putchar(i);
-	}
 	_putchar('\n');
-	return (0);
 }
