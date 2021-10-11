de4a916fa342844be4a0c0c179dafa33cdf3b0b4
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-11 10:36:07 +0200

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line
- * @str: pointer to string
+ * @s: pointer to string
  */
 
 void print_rev(char *s)
@@ -13,4 +13,20 @@ void print_rev(char *s)
 	for (c = len - 1; c >= 0; c--)
 		_putchar(s[c]);
 	_putchar('\n');
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
 }
