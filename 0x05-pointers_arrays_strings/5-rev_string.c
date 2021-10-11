2f1fa927fd0448d3a4d295c2b30b2573c0d305bf
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-11 10:52:57 +0200

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: pointer to string
+ */
+
+void rev_string(char *s)
+{
+	int len = _strlen(s);
+	int n = _strlen(s) - 1;
+	int c;
+	int d;
+	char array[1000];
+
+	for (d = 0; d < len; d++, n--)
+		array[d] = s[n];
+	for (c = 0; c < len; c++)
+		s[c] = array[c];
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to string
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+		c++;
+	return (c);
+}
