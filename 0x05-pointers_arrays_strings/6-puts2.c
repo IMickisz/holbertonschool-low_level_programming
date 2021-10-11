9ff71ccbe819962c5f017b4b0899e1e5ab08e23a
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-11 11:14:12 +0200

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string
+ * @str: pointer to string
+ */
+
+void puts2(char *str)
+{
+	int c;
+
+	for (c = 0; str[c] != '\0'; c + 2)
+		_putchar(str[c]);
+	_putchar('\n');
+}
