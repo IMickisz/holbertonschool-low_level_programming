b9d7006d0d138430ddbecc094b1a04bf8069f415
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-13 13:11:10 +0200

    task 0

---
+++ 0x06-pointers-arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to a string
+ * @src: pointer to a string
+ * Return: the pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return(strcat(dest, src));
+}
