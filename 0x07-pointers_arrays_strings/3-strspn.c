46e176ce722b88ddc77f409ce2eff8420edbdd66
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-19 11:20:20 +0200

    task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,13 +14,12 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
-		{
-			if (s[i] == accept[j])
-				sum++;
-			else
-				break;
-		}
+		for (j = 0; accept[j] != '\0' && accept[j] != s[i]; j++)
+			;
+		if (s[i] == accept[j])
+			sum++;
+		if (accept[j] == '\0')
+			return (sum);
 	}
 	return (sum);
 }
