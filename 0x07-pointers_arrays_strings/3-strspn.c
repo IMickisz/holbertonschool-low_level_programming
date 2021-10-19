1fa5ae86abe59549db13c973f9bd68044661f0dd
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-19 11:09:51 +0200

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: character array to check
+ * @accept: character array to check for
+ * Return: the number of bytes from accept
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int sum;
+	int i, j;
+
+	for (i = 0; s[i] != ' '; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				sum++;
+		}
+	}
+	return (sum);
+}
