62efb48dcdcfa0c22c4dc1c2f9a1f1d0254438fa
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-21 23:23:48 +0200

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "mainn.h"
+
+/**
+ * _pow_recursion - function that returns value of x raised to y
+ * @x: base
+ * @y: power
+ * Return: the value of x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * (_pow_recursion(x, y - 1)));
+}
