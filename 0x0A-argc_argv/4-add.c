da07f086707a9c1a33b3e1fb39e80a3d2b927020
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-22 12:56:03 +0200

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,18 +19,8 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != 0 ; j++)
-		{
-			if (argv[i][j] > 47 && argv[i][j] < 58)
-				continue;
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			if (!atoi(argv[i]))
-				sum += atoi(argv[i]);
-		}
+		if (!atoi(argv[i]))
+			sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 	return (0);
