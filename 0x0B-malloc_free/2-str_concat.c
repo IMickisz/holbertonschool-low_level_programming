65a2d04e75f1d6bdf2829e290e2ceb57ab741fbf
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-26 23:59:19 +0200

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to newly allocated space
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	unsigned int i, j, k, p, combolen;
+	unsigned int len1 = 0, len2 = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (j = 0; *(s1 + j) != '\0'; j++)
+		len1++;
+	for (k = 0; *(s2 + k) != '\0'; k++)
+		len2++;
+	combolen = len1 + len2;
+	a = malloc((combolen + 1) * sizeof(char));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		*(a + i) = *(s1 + i);
+	p = 0;
+	for (j = len1; j < combolen; j++;)
+	{
+		*(a + j) = *(s2 + p);
+		p++;
+	}
+	*(a + combolen) = '\0';
+	return (a);
+}
