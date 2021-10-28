29428525de1ba3147dd4e0fb091678135b2313d8
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-28 12:52:14 +0200

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < len1; i++)
 		s[i] = s1[i];
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 		s[len1 + j] = s2[j];
 	s[len1 + j] = '\0';
 	return (s);
