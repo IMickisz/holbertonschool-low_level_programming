967ec171d5e3bde4c9dbfc89e0cb9a056bb3d243
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-28 22:48:09 +0200

    task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,13 +5,6 @@
  * _realloc - function that reallocates a memory block using malloc and free
  * The contents will be copied to the newly allocated space, in the range
  * from the start of ptr up to the minimum of the old and new sizes
- * If new_size > old_size, the “added” memory should not be initialized
- * If new_size == old_size do not do anything and return ptr
- * If ptr is NULL, then the call is equivalent to malloc(new_size),
- * for all values of old_size and new_size
- * If new_size is equal to zero, and ptr is not NULL, then the call
- * is equivalent to free(ptr). Return NULL
- * Don’t forget to free ptr when it makes sense
  * @ptr: pointer to the memory previously allocated with a call
  * to malloc: malloc(old_size)
  * @old_size: size, in bytes, of the allocated space for ptr
@@ -25,91 +18,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
-	       	ptr = malloc(new_size);
+		ptr = malloc(new_size);
 		if (ptr == NULL)
 			return (NULL);
 		return (ptr);
 	}
-}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-
 	dest = malloc(new_size);
 	if (dest == NULL)
 		return (NULL);
-
 	src = ptr;
-
 	for (i = 0; i < new_size && i < old_size; i++)
 		dest[i] = src[i];
 	free(ptr);
-
 	return (dest);
-}
-
-/**
- * _memcpy - copies memory area.
- * @dest: memory area to overwrite
- * @src: string to overwrite on dest
- * @n: numbers of bytes from memory area
- * Return: char dest
- */
-
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int cont = 0;
-
-	while (cont < n)
-	{
-		dest[cont] = src[cont];
-		cont++;
-	}
-	return (dest);
-}
-/**
- * _realloc -  function that reallocates a memory block using malloc and free
- * @ptr:pointer to the memory previously allocated with a call to
- * malloc: malloc(old_size)
- * @old_size: size, in bytes, of the allocated space for ptr
- * @new_size: new size, in bytes of the new memory block
- *
- * Return: pointer to address reallocate
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	void *p;
-
-	if (new_size == old_size)
-		return (ptr);
-
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-
-	if (ptr == NULL)
-	{
-		p = malloc(new_size);
-		if (p == NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
-		free(ptr);
-		return (p);
-	}
-	p = malloc(new_size);
-	if (p == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (new_size > old_size)
-		_memcpy(p, ptr, old_size);
-	free(ptr);
-	return (p);
 }
