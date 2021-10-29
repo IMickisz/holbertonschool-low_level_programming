ed88010eefce087c7494c7498faaab6a01d7556e
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-10-29 10:59:52 +0200

    task 5

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -24,10 +24,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		nptr = malloc(new_size);
 		if (nptr == NULL)
 		{
-			free (nptr)
+			free (nptr);
 			return (NULL);
 		}
-		free (ptr)
+		free (ptr);
 		return (nptr);
 	}
 	if (new_size == 0)
