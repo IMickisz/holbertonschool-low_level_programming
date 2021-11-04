f99e99812d64fd7346db9cbb100fee2b78cc448a
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-04 21:14:57 +0100

    task 4

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,13 +1,17 @@
-section .data
-
-    message db "Hello, world!", 10
-
 section .text
+global main
 
-global _start
-_start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, message
-    mov rdx, 14
-    syscall
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
