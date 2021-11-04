30a6a65b5e189b2028dc4f0fe2daff50a53c1e1e
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-04 13:29:49 +0100

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * print_name - prints a name given a function
@@ -10,7 +9,6 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name != NULL || f != NULL)
+		f(name);
 }
