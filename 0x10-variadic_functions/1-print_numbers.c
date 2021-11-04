c0b4ad0bf426a154d62317f62a46d236ccd3b43e
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-04 12:54:03 +0100

    task 2

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i != n - 1)
-			printf("%s", va_arg(args, int), separator);
+			printf("%s", separator);
 	}
 	va_end(args);
 	putchar('\n');
