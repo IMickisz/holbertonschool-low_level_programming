b23823158130b5d237b7e70fbcba72379f1a14e5
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-16 09:32:35 +0100

    added struct

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,7 @@
 #ifndef MORE_SINGLY_LINKED_LIST
 #define MORE_SINGLY_LINKED_LIST
+#include <stdio.h>
+#include <stddef.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -8,8 +10,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
