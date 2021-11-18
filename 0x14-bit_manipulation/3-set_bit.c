df2ce0bb3600108b22e6eab5208a926d606c414a
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-18 15:13:16 +0100

    added task 3, rm error task 2 corrected

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to a decimal to convert
+ * @index: index, starting from 0 of the bit you want to set
+ * Return: 1 if it worked, or -1 if error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || index >= 64)   /*size of unsigned long int*/
+		return (-1);
+	else
+		*n = ((1 << index) | *n);
+	return (1);
+}
