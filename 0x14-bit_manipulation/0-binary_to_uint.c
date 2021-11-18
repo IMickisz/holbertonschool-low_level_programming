50ba5b4450be1022d69ac7b34edeeb0974c892ec
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-18 11:57:49 +0100

    corrected betty, removed atoi

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,25 +10,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0, len, i, binary, rest, base = 1;
+	unsigned int dec = 0;
+	int len, power = 1;
 
 	if (b == NULL)
 		return (0);
 	len = _strlen(b);
-	for (i = 0; len > i; i++)
+	for (len -= 1; len >= 0; len--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
 			return (0);
+		dec +=  (b[len] - '0') * power;
+		power *= 2;
 	}
-	binary = _atoi(b);
-	while (binary > 0)
-	{
-		rest = binary % 10;
-		sum += rest * base;
-		binary /= 10;
-		base *= 2;
-	}
-	return (sum);
+	return (dec);
 }
 
 /**
@@ -44,40 +39,4 @@ int _strlen(const char *s)
 	for (c = 0; s[c] != '\0'; c++)
 		;
 	return (c);
-}
-
-/**
- * _atoi - Convert a string to integer.
- * @s: char array string
- * Description: Number in the string can be preceded by an infinite
- * number of characters.
- * You need to take into account all -/+ signs before the number.
- * If there are no numbers in the string, return 0.
- * No need to check for overflow.
- * Not allowed to hard-code special values.
- * Return: first integer found in string
- */
-
-int _atoi(const char *s)
-{
-	int i;
-	int h = 0, p = -1;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == '-')
-			p *= -1;
-		if (s[i] > 47 && s[i] < 58)
-		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
-		}
-	}
-	if (p < 0)
-		h *= -1;
-	return (h);
 }
