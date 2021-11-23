1d84180de4f983166f0fe98977edf7e2a8016d81
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-23 15:21:34 +0100

    added task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ * @argc: the number of arguments
+ * @argv: an array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int from, to, r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (from == -1 || r == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	w = write(to, buffer, r);
+	if (w != r || w == -1 || to == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	close_file(from);
+	close_file(to);
+	return (0);
+}
+
+/**
+ * close_file - Closes file descriptors.
+ * @file: The file descriptor to be closed.
+ */
+
+void close_file(int file)
+{
+	int c;
+
+	c = close(file);
+	if (c == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		exit(100);
+	}
+}
