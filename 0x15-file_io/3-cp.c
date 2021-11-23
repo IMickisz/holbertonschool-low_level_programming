9c281aff377a9de6d64d61c1d7beccaf8b457784
Ivan Mickisz <ivan.mickisz3@gmail.com>
2021-11-23 15:51:24 +0100

    corrected betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,11 +25,11 @@ int main(int argc, char *argv[])
 	from = open(argv[1], O_RDONLY);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (from == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	while ((r = read(from, buffer, 1024)) > 0)
 	{
 		w = write(to, buffer, r);
@@ -42,11 +42,11 @@ int main(int argc, char *argv[])
 		to = open(argv[2], O_WRONLY | O_APPEND);
 	}
 	if (from == -1 || r == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
 	close_file(from);
 	close_file(to);
