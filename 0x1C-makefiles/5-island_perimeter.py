f11bc208a6d24635f6769111143f8f3a38d2447a
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-03-17 14:05:26 +0100

    corrected typo

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,15 +12,15 @@ def island_perimeter(grid):
     lenght = len(grid) - 1
     width = len(grid[0]) -1
 
-    for i in range(lenght):
-        for j in range(width):
-            if grid[i][j] == 1:
-                if grid[i - 1][j] == 0:
+    for i, value in enumerate(grid):
+        for j, val in enumerate(value):
+            if val == 1:
+                if i == 0 or grid[i - 1][j] != 1:
                     perimeter += 1
-                if grid[i][j - 1] == 0:
+                if j == 0 or grid[i][j - 1] != 1:
                     perimeter += 1
-                if grid[i + 1][j] == 0:
+                if j == width or grid[i][j + 1] != 1:
                     perimeter += 1
-                if grid[i][j + 1] == 0:
+                if i == lenght or grid[i + 1][j] != 1:
                     perimeter += 1
     return perimeter
